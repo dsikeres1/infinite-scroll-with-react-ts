cb298ac20511e9adb0b026077321fd68dc206eff
DSikeres1 <dsikeres1@gmail.com>
2021-04-16 14:03:38 +0900

    a] intersection_observer/InfiniteScroll.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-import ScrollEventListenerInfiniteScroll from './components/scroll_event_listener/InfiniteScroll';
+// import ScrollEventListenerInfiniteScroll from './components/scroll_event_listener/InfiniteScroll';
+import IntersectionObserverInfiniteScroll from './components/intersection_observer/InfiniteScroll';
 
 function App() {
-  return <ScrollEventListenerInfiniteScroll />;
+  // return <ScrollEventListenerInfiniteScroll />;
+  return <IntersectionObserverInfiniteScroll />;
 }
 
 export default App;
---
+++ src/components/intersection_observer/InfiniteScroll.tsx
@@ -0,0 +1,43 @@
+// InfiniteScroll.tsx
+import React, { useState, useCallback, useEffect, useRef } from 'react';
+import { getPostList, postType } from '../../model/postList';
+
+import '../InfiniteScroll.css';
+
+const InfiniteScroll = (): JSX.Element => {
+  const loader = useRef<any>(null);
+  const [page, setPage] = useState<number>(1);
+  const [posts, setPosts] = useState<postType[]>(getPostList(1));
+  const handleObserver = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      if (entries[0].isIntersecting) {
+        setPosts(posts.concat(getPostList(page + 1)));
+        setPage(page + 1);
+      }
+    },
+    [page, posts],
+  );
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver, { threshold: 1 });
+    observer.observe(loader.current);
+
+    return () => observer && observer.disconnect();
+  }, [handleObserver]);
+
+  return (
+    <div className="container">
+      {posts.map((post: postType, idx: number) => (
+        <div className="postItem" key={idx}>
+          <h3>{post.title}</h3>
+          {post.contents}
+          <br />
+          {post.page} 페이지
+        </div>
+      ))}
+      <div ref={loader} />
+    </div>
+  );
+};
+
+export default InfiniteScroll;
